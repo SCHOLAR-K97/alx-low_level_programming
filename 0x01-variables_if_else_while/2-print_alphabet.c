6028972d7d6435c5a3ac8d7d21d74b083b4e1276
SCHOLAR-K97 <scholarsticakeremu@gmail.com>
2022-10-17 14:40:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entire line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a'
+	while
+		(c <= 'x')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
